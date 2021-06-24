4cd4ecae2857fa363fa31df5f4da4aaffe2ef119
Harsh <harsh.mittal@hotstar.com>
2021-06-24 13:51:26 +0530

    Pushing a dev branch

--- src/helloApp/app.py
+++ src/helloApp/app.py
@@ -1,8 +1,8 @@
 import requests
 
 def main():
-    r = requests.get('https://www.google.com/')
-    print("Google said "+ str(r.status_code))
+  #  r = requests.get('https://www.google.com/')
+    print("Google said 500")
 
 if __name__=='__main__':
     main()
